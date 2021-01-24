4c8d8e54f469c22eb3cbd6f3e6aacce8cc853e8d
Albumen Kevin <jhq0812@gmail.com>
2021-01-24 13:15:27 +0800

    pr4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code
-        System.out.println("Demo AAAAA");
+        System.out.println("Demo AAAAAAAA");
     }
 }
